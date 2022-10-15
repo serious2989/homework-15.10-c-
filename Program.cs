fddc9a1cef35ac7d85b8076ef52b3f453fd2d1e9
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-15 15:17:04 +0300

    возведение в степень

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int Exponentiation(int A, int B)
+{
+  int result = 1;
+  for(int i = 1; i <= B; i++)
+  {
+    result = result * A;
+  }
+    
+    return result;
+}
+
+  Console.Write("Введите число A: ");
+  int A = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int B = Convert.ToInt32(Console.ReadLine());
+
+  int result = Exponentiation(A, B);
+  Console.WriteLine("Ответ: " + result);
