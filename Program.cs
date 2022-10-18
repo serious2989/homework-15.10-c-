b0c5f1334899b05ad7ea96408cc8fc8b74c3f45a
Dmitriy Vasilyev <serious29042008@yandex.ru>
2022-10-18 11:57:06 +0300

    second max*

--- Program.cs
+++ Program.cs
@@ -1,27 +1,19 @@
-﻿// task 38
+﻿
+// нахождение второго максимума 
+
 Console.Clear();
-void InputArray(double[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-        array[i] = Math.Round(new Random().NextDouble() * 10, 2);
-}
-
-double CountEventNumbers(double[] array)
-{
-    double minArray = array[0], maxArray = array[0];
-    for (int i = 1; i < array.Length; i++)
-    {
-        if (maxArray < array[i])
-            maxArray = array[i];
-        else if (minArray > array[i])
-            minArray = array[i];
-    }
-    return maxArray - minArray;
-}
-
-Console.Write("Введите кол-во элементов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
-double[] array = new double[n];
-InputArray(array);
-Console.WriteLine($"[{string.Join(", ", array)}]");
-Console.WriteLine($"Разница между макс и мин: {CountEventNumbers(array)}");
+int firstMax = n, secondMax = -1;
+
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > firstMax)
+    {
+        secondMax = firstMax;
+        firstMax = n;
+    }
+    else if (n > secondMax)
+        secondMax = n;
+}
+Console.WriteLine(secondMax);
